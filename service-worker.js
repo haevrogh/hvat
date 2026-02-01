5a866fbd1437392a0db880e2db64bf4d4c7737fa
haevrogh <khasanov.er@gmail.com>
2026-02-02 00:02:30 +0500

    Restore offline cache in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,26 @@
+const CACHE_NAME = 'hvat-static-v1';
+const PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/styles/main.css',
+  '/js/finders.js',
+  '/js/formulas.js',
+  '/js/init.js',
+  '/js/ui.js',
+  '/icons/icon.svg',
+];
+
 self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))),
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => (key === CACHE_NAME ? null : caches.delete(key))))),
   );
   self.clients.claim();
 });
@@ -15,6 +31,15 @@ self.addEventListener('fetch', (event) => {
   }
 
   event.respondWith(
-    fetch(event.request, { cache: 'no-store' }).catch(() => caches.match(event.request)),
+    fetch(event.request, { cache: 'no-store' })
+      .then((response) => {
+        if (!response || response.status !== 200) {
+          return response;
+        }
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
   );
 });
