258194e90251c24b8fc5980ece3275227e3f7732
haevrogh <khasanov.er@gmail.com>
2026-02-01 22:53:41 +0500

    Replace binary icons with SVG for PWA

--- js/init.js
+++ js/init.js
@@ -26,3 +26,11 @@ function runTests() {
 
 initUI();
 runTests();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js').catch((error) => {
+      console.warn('Service worker registration failed', error);
+    });
+  });
+}
